06cb274b1cc0d7786c30f1b273305fff0dfd700f
Holden <34213478+hodlen@users.noreply.github.com>
2023-11-03 17:04:38 +0800

    fix: global side effect on logging

--- gptcache/utils/log.py
+++ gptcache/utils/log.py
@@ -2,7 +2,7 @@ import logging
 
 import gptcache
 
-FORMAT = '%(asctime)s - %(thread)d - %(filename)s-%(module)s:%(lineno)s - %(levelname)s: %(message)s'
-logging.basicConfig(format=FORMAT)
+# FORMAT = '%(asctime)s - %(thread)d - %(filename)s-%(module)s:%(lineno)s - %(levelname)s: %(message)s'
+# logging.basicConfig(format=FORMAT)
 
 gptcache_log = logging.getLogger(f'gptcache:{gptcache.__version__}')
